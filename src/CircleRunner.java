d2fb998464edb8b7cee66029564f8f2ed51b9e03
ronanm14 <ronanm14@nycstudents.net>
2023-09-28 12:29:45 -0400

    circkle

--- src/CircleRunner.java
+++ src/CircleRunner.java
@@ -1,12 +1,12 @@
 public class CircleRunner {
     public static void main(String[] args) {
         // A. create a Circle object with radius 5.0
-
+        Circle Circle1 = new Circle(5.0);
         // B. locate the method defined in the Circle class and write
         //    a line of code to call it on your object
-
+        Circle1.printArea();
         // C. BEFORE running this code, PREDICT what gets printed
-
+        
         // D. run the code and confirm your prediction! Were you correct?
     }
 }
